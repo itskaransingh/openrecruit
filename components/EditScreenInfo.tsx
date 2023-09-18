33b05b0ebf9b764efcc7290a36b09ec8e9b1c021
Karan Singh <karanrsingh2662006@gmail.com>
2023-09-18 19:19:44 +0530

    initial commit

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -15,7 +15,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          Open up the code for this screen and edit it:
         </Text>
 
         <View
